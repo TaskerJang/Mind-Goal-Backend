5a8997d05cbc6c49bd244da761e0e0c2d52f3893
JJangcoding <bymyself103@naver.com>
2024-12-01 18:49:33 +0900

    init: 프로젝트 초기 설정 및 디렉토리 구조 생성

---
+++ src/main/java/com/mindgoal/backend/MindGoalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindgoal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindGoalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindGoalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindgoal/backend/MindGoalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindgoal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindGoalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
